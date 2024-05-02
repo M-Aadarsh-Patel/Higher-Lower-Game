06d08066b031509fa65f252164125214e2374810
M Aadarsh Patel <aadarsh9963355519@gmail.com>
2024-05-02 13:27:51 +0530

    I could only think of adding one function

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ import random
 from game_data import data
 from art import logo, vs
 
+A = random.choice(data)
+B = random.choice(data)
+
+def Info_to_user(celebrety1, celebrety2):
+    print(f"Compare A: {celebrety1['name']}, a {celebrety1['description']} from {celebrety1['country']}")
+    print(f"{celebrety1['follower_count']}")
+    print(vs)
+    print(f"Against B: {celebrety2['name']}, a {celebrety2['description']} from {celebrety2['country']}")
+    print(f"{celebrety2['follower_count']}")
+
 
 def game():
     print(logo)
 
-    A = random.choice(data)
-    B = random.choice(data)
-
     score = 0
 
     while True:
-        print(f"Compare A: {A['name']}, a {A['description']} from {A['country']}")
-        print(f"{A['follower_count']}")
-        print(vs)
-        print(f"Against B: {B['name']}, a {B['description']} from {B['country']}")
-        print(f"{B['follower_count']}")
 
+        Info_to_user(celebrety1=A, celebrety2=B)
         guess = input("Who has more followers A or B: ")
 
         if guess.lower() == 'a':
